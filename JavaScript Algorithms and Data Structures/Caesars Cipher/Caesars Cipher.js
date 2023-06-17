bf773ff8eedf439242374538c634ffbcda475552
Danut Nanu <125583127+danutnanu@users.noreply.github.com>
2023-06-17 10:01:50 +0100

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/Caesars Cipher/Caesars Cipher.js
@@ -0,0 +1,5 @@
+function rot13(str) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+      return str.replace(/[a-z]/gi, letter => alpha[alpha.indexOf(letter) + 13]);
+    }
+    rot13("SERR PBQR PNZC");
